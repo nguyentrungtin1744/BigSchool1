26d6a83d49b2760c0ff2156342889731183b9095
nguyentrungtin1744 <49141224+nguyentrungtin1744@users.noreply.github.com>
2019-04-10 15:41:36 +0700

    BigSchool moi

---
+++ BigSchool1/Controllers/CoursesController.cs
@@ -0,0 +1,36 @@
+﻿using BigSchool1.Models;
+using BigSchool1.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool1.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            var course = new Course
+            {
+                LecturerId=User.Identity.GetUserId(),
+                DateTime=viewModel.GetDateTime(),
+                CategoryId=viewModel.Category,
+                Place=viewModel.Place
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
---
+++ BigSchool1/Models/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BigSchool1.Models
+{
+    public class Course
+    {
+        internal object LecturerId;
+
+        public int Id { get; set; }
+        public ApplicationUser Lecturer { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Place { get; set; }
+
+        public DateTime DateTime { get; set; }
+        [Required]
+        public byte CategoryId { get; set; }
+    }
+}
